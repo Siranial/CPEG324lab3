adb489cd30ffb90fb0d5c2d01576a6b305e0adb9
Siranial <jonadams@udel.edu>
2022-04-27 22:24:28 -0400

    Created and removed files

---
+++ Part2/comparator.vhdl
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity comparator is
+    Port (  A,B : in STD_LOGIC_VECTOR (7 downto 0);
+            S : in STD_LOGIC;
+            Y : out STD_LOGIC_vector (7 downto 0));
+end comparator;
+
+architecture Behavioral of comparator is
+
+begin
+
+    -- Logic
+
+end Behavioral;
